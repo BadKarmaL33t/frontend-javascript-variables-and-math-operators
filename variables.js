f3ccca4b15b1935e41697ab7f012c6668dc6e6eb
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-21 17:45:37 +0200

    opdracht uitwerken

--- variables.js
+++ variables.js
@@ -7,38 +7,38 @@ const start = "Let's go!";
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const name = "Jack";
+console.log(name);
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const eyeColor = "Blue";
+console.log(eyeColor);
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+const age = 7;
+console.log(age);
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+const profession = "Couchpotato";
+console.log(profession);
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const shoesize = 10;
+console.log(shoesize);
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+const hasPartner = true;
+console.log(hasPartner);
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+const numberOfChildren = 0;
+console.log(numberOfChildren)
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
